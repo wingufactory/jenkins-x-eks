07b33acde7105fad7f4c188d9b35b5736c6c9971
Kalidou SY <ext.kalidou.sy@sncf.fr>
2024-01-07 03:25:51 +0100

    fix: configure cluster repository and project

--- variables.tf
+++ variables.tf
@@ -45,7 +45,7 @@ variable "is_jx2" {
 variable "jx_git_url" {
   description = "URL for the Jenins X cluster git repository"
   type        = string
-  default     = "https://github.com/wingufactory/jenkins-x-eks-vault.git" 
+  default     = "https://github.com/wingufactory/jenkins-x-eks-asm.git" 
 }
 
 variable "jx_bot_username" {
